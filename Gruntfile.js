0094c9d35286ceca9e20451d137ebfadd3d6688d
libjared <jared@perrycode.com>
2016-06-07 11:46:04 -0400

    eslint part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,4 +43,5 @@ module.exports = function(grunt) {
     });
     
     require('load-grunt-tasks')(grunt);
+    grunt.registerTask('default', ['eslint', 'bower_concat', 'sasslint', 'sass', 'copy']);
 };
